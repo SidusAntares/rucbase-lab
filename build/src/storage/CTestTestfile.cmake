0c0e703f3efa6c31dcbd8cc73d6485aa376263c5
SidusAntares <2934978005@qq.com>
2026-06-07 16:25:26 +0800

    chore: ignore local build files

--- build/src/storage/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sidusantares/data/rucbase-lab/src/storage
-# Build directory: /home/sidusantares/data/rucbase-lab/build/src/storage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
